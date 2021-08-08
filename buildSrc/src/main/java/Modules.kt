b295eaabad981d0dd475c63db82e6fcecb0a88ee
Heman Parbhakar <parbhakar.heman@gmail.com>
2021-08-08 12:03:38 +0530

    Added Dependices

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,18 @@
+object Modules {
+
+    const val app = ":app"
+
+    const val core = ":core"
+
+    const val components = ":components"
+
+    const val constants = ":constants"
+
+    const val weather = ":weather"
+    const val weatherDataSource = ":weather:weather-datasource"
+    const val weatherDataSourceTest = ":weather:weather-datasource-test"
+    const val weatherDomain = ":weather:weather-domain"
+    const val weatherInteractors = ":weather:weather-interactors"
+    const val ui_weatherDetail = ":weather:ui-weatherDetail"
+    const val ui_weatherList = ":weather:ui-weatherList"
+}
